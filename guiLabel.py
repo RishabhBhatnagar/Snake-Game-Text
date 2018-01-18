5c73f61d4fe19f7d5a2680dbab45cc208e45365a
vinay-deshmukh <vinay_deshmukh@outlook.com>
2018-01-18 19:42:59 +0530

    Can modify text after creation

--- guiLabel.py
+++ guiLabel.py
@@ -7,4 +7,7 @@ root.geometry("400x400+100+100")
 my_text = Label(root, text="Hello World", anchor='nw')#justify="left")
 my_text.pack(fill='both')
 
+
+my_text.config(text='New Words')
+
 root.mainloop()
