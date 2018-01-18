5d6e9f10941f657662d5c6797e9dda50c7756afa
vinay-deshmukh <vinay_deshmukh@outlook.com>
2018-01-18 21:19:58 +0530

    Modularized, separated into functions

--- guiLabel.py
+++ guiLabel.py
@@ -1,33 +1,44 @@
 from tkinter import Tk, Label
-from textBG import textBoard
+from textRes import textBoard
 from tkinter.font import Font
 
-root = Tk()
 
-# Setting size
-root.geometry("400x400+100+100")
+def create_labelBoard(root):
 
-labelBoard = Label(root, text="Hello World", anchor='nw')#justify="left")
-labelBoard.pack(fill='both')
+    labelBoard = Label(root, text="Hello World", anchor='nw')
+    labelBoard.pack(fill='both')
 
 
-#Custom font
-my_font = Font(family='Consolas',size=15)
+    #Custom font
+    my_font = Font(family='Consolas',size=15)
 
-labelBoard.config(text='New Words')
+    labelBoard.config(text='New Words')
 
-# Setting the font(monospace)
-labelBoard.config(font=my_font)
+    # Setting the font(monospace)
+    labelBoard.config(font=my_font)
 
-# Setting the text bg
-labelBoard.config(text=textBoard)
+    # Setting the text bg
+    labelBoard.config(text=textBoard)
 
-labelBoard.config(bg='red')
-labelBoard.config(highlightthickness=6)
-labelBoard.config(relief='solid')
+    labelBoard.config(bg='red')
+    labelBoard.config(highlightthickness=6)
+    labelBoard.config(relief='solid')
 
 
-# Setting location in grid
-labelBoard.grid(row=0, column=0)
+    # Setting location in grid
+    labelBoard.grid(row=0, column=0)
 
-root.mainloop()
+
+
+def create_root():
+    root = Tk()
+
+    # Setting size
+    root.geometry("400x400+100+100")
+
+    create_labelBoard(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    create_root()
+
