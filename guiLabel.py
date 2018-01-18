cf3d33ce7e73419d6836ef77a10138a78a279319
vinay-deshmukh <vinay_deshmukh@outlook.com>
2018-01-18 21:29:17 +0530

    Formatted and commented nice

--- guiLabel.py
+++ guiLabel.py
@@ -5,29 +5,25 @@ from tkinter.font import Font
 
 def create_labelBoard(root):
 
+    # Creating the labelBoard
     labelBoard = Label(root, text="Hello World", anchor='nw')
     labelBoard.pack(fill='both')
 
-
-    #Custom font
-    my_font = Font(family='Consolas',size=15)
-
-    labelBoard.config(text='New Words')
-
     # Setting the font(monospace)
+    # Custom font
+    my_font = Font(family='Consolas', size=15)
     labelBoard.config(font=my_font)
 
-    # Setting the text bg
+    # Setting the text
     labelBoard.config(text=textBoard)
 
+    # Set background color as red
     labelBoard.config(bg='red')
     labelBoard.config(highlightthickness=6)
     labelBoard.config(relief='solid')
 
-
     # Setting location in grid
     labelBoard.grid(row=0, column=0)
-
 
 
 def create_root():
@@ -37,8 +33,11 @@ def create_root():
     root.geometry("400x400+100+100")
 
     create_labelBoard(root)
+
+    # Runs the window
     root.mainloop()
 
-if __name__ == '__main__':
-    create_root()
 
+if __name__ == '__main__':
+
+    create_root()
