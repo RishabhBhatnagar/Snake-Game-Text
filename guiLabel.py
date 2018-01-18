a113823c682cad682c09b5404be5ccff7331aaaa
vinay-deshmukh <vinay_deshmukh@outlook.com>
2018-01-18 20:06:41 +0530

    Added textBG to label

--- guiLabel.py
+++ guiLabel.py
@@ -1,4 +1,7 @@
 from tkinter import Tk, Label
+from textBG import textBoard
+from tkinter.font import Font
+
 root = Tk()
 
 # Setting size
@@ -8,6 +11,17 @@ my_text = Label(root, text="Hello World", anchor='nw')#justify="left")
 my_text.pack(fill='both')
 
 
+#Custom font
+my_font = Font(family='Consolas',size=15)
+
 my_text.config(text='New Words')
+
+# Setting the font(monospace)
+my_text.config(font=my_font)
+
+# Setting the text bg
+my_text.config(text=textBoard)
+
+my_text.config(highlightcolor='red', highlightthickness=6, relief='solid')
 
 root.mainloop()
