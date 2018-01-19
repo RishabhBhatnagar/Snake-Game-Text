450b74249db067a7c211f5792973be16d5306d45
vinay-deshmukh <vinay_deshmukh@outlook.com>
2018-01-19 22:27:39 +0530

    Reverted to Finished 1), eafcb6d

--- guiMain.py
+++ guiMain.py
@@ -1,28 +1,7 @@
 from tkinter import Tk, Label
 import textRes
 from tkinter.font import Font
-import gameLogic
 
-labelBoard = ''
-
-def keyA(event, labelBoard):
-    print("a pressed")
-    
-    lent_before_newline = labelBoard['text'].index('\n')
-
-    # check image
-    # since newline index will directly give the correct leng of row
-    newString = labelBoard['text']
-
-    for x, y in gameLogic.snakeList:
-        # labelBoard['text'][y*lent_before_newline + (x+1)] = "#"
-        locationForNewCharacter = int(y*lent_before_newline + (x+1))
-        print(locationForNewCharacter)
-        newString = newString[:locationForNewCharacter] \
-            + "#" + newString[locationForNewCharacter+1:]
-
-    # Set text of label
-    labelBoard.config(text=newString)
 
 def create_labelBoard(root):
 
@@ -51,18 +30,12 @@ def create_labelBoard(root):
     # Setting location in grid
     labelBoard.grid(row=0, column=0)
 
-    # Trying updating snake on grid
-    root.bind('a', lambda event, obj=labelBoard: keyA(event, obj))
 
-
-    return labelBoard
-
-
-def create_root(root):
-    # root = Tk()
+def create_root():
+    root = Tk()
 
     # Setting size
-    # root.geometry("400x400+100+100")
+    root.geometry("400x400+100+100")
 
     create_labelBoard(root)
 
@@ -72,5 +45,4 @@ def create_root(root):
 
 if __name__ == '__main__':
 
-    root = Tk()
-    create_root(root)
+    create_root()
